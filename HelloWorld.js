874c957b10dc1a56ad43381237436096c7241396
Rob Brodie <54519499+FindRobBrodie@users.noreply.github.com>
2020-03-23 23:39:10 -0400

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,10 @@
 Hello World!
 
+/*BRO-LOOP:{groups}*//*BRO:{group}*/
+
+/*BRO-LOOP:{entities}*//*BRO:{entity}*/
+/*BRO-LOOP:{fields}*/-/*BRO:{name}*/ /*BRO:{type}*/ /*BRO:{width}*/
+/*END-BRO-LOOP:{fields}*/
+/*END-BRO-LOOP:{entities}*//*END-BRO-LOOP:{groups}*/
+  
+end example
